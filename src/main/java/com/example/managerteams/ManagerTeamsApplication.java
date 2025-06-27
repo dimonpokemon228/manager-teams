98fb576e5bf62f57a990f963296fef7482158f40
Vasta1921 <vanvasta21@gmail.com>
2025-06-28 00:24:19 +0300

    Первый раз

---
+++ src/main/java/com/example/managerteams/ManagerTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.managerteams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerTeamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerTeamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/managerteams/ManagerTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managerteams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerTeamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
