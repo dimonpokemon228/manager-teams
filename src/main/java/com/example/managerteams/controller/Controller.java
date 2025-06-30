d46fb9d83fdc1896729a720b4815edec9343b3cb
Vasta1921 <vanvasta21@gmail.com>
2025-06-30 19:32:35 +0300

    test with builder

--- src/main/java/com/example/managerteams/controller/Controller.java
+++ src/main/java/com/example/managerteams/controller/Controller.java
@@ -1,20 +1,20 @@
-//package com.example.managerteams.controller;
-//
-//import com.example.managerteams.model.dto.CreatePlayerDto;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequestMapping("/")
-//@RestController
-//@RequiredArgsConstructor
-//public class Controller {
-//
-////    @PostMapping
-////    public CreatePlayerDto saveClubAndPlayer(@RequestBody CreatePlayerDto createplayerdto){
-////
-////    return Player.
-//}
-//}
+package com.example.managerteams.controller;
+
+import com.example.managerteams.model.dto.CreatePlayerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+
+    @PostMapping
+    public CreatePlayerDto saveClubAndPlayer(@RequestBody CreatePlayerDto createplayerdto){
+
+    return CreatePlayerDto.builder().build();
+}
+}
--- src/main/java/com/example/managerteams/mapper/Mapper.java
+++ src/main/java/com/example/managerteams/mapper/Mapper.java
@@ -1,16 +1,15 @@
-//package com.example.managerteams.mapper;
-//
-//import com.example.managerteams.model.dto.CreatePlayerDto;
-//import com.example.managerteams.model.entity.Player;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Service;
-//
-////@RequiredArgsConstructor
-////@Service
-////public class Mapper {
-////    public Player mapPlayerFromDto(CreatePlayerDto createPlayerDto){
-////
-////    }
-//
-//}
+package com.example.managerteams.mapper;
+
+import com.example.managerteams.model.dto.CreatePlayerDto;
+import com.example.managerteams.model.entity.Player;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class Mapper {
+    public Player mapPlayerFromDto(CreatePlayerDto createPlayerDto){
+        return Player.builder().build();
+    }
+
+}
